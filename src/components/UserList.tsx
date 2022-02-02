f6a2032abcab6c56634f1965d26aa14466f4d903
ermilla <wteffera11@gmail.com>
2022-02-02 11:38:12 +0300

    Filter and sort impelemented for User list

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,18 +1,63 @@
-import React from "react";
-import { Grid, GridColumn } from "@progress/kendo-react-grid";
+import React, { useCallback, useState } from "react";
+import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
 import { observer } from "mobx-react";
 import store from "../mobx/store";
+import {
+  CompositeFilterDescriptor,
+  orderBy,
+  filterBy,
+  SortDescriptor,
+} from "@progress/kendo-data-query";
 
 interface UserListProps {}
 
-console.log(store.users);
+// interface SortType {
+//   field: string;
+//   dir: string;
+// }
+// interface
+// interface FilterType {
+//     logic: string;
+//     filters: CompositeFilterDescriptor[];
+// }
+
 const UserList: React.FC<UserListProps> = ({}) => {
+  const initialSort: SortDescriptor[] = [
+    {
+      field: "Username",
+      dir: "asc",
+    },
+  ];
+  const initialFilter: CompositeFilterDescriptor = {
+    logic: "and",
+    filters: [
+      {
+        field: "Username",
+        operator: "contains",
+        value: "",
+      },
+    ],
+  };
+
+  const [currentSort, setSort] = useState(initialSort);
+  const [filter, setFilter] = useState(initialFilter);
+
   return (
-    <Grid data={store.users}>
-      <GridColumn field="Username" title="User Name" />
-      <GridColumn field="FullName" title="Full Name" />
-      <GridColumn field="LastLogin" title="Last Login" />
-      <GridColumn field="Enabled" />
+    <Grid
+      data={orderBy(filterBy(store.users, filter), currentSort)}
+      sortable={true}
+      filterable={true}
+      sort={currentSort}
+      filter={filter}
+      onSortChange={(e) => {
+        setSort(e.sort);
+      }}
+      onFilterChange={(e) => setFilter(e.filter)}
+    >
+      <Column field="Username" title="User Name" />
+      <Column field="FullName" title="Full Name" />
+      <Column field="LastLogin" title="Last Login" />
+      <Column field="Enabled" />
     </Grid>
   );
 };
